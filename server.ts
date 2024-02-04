bb3725cadfbb20f769179877aea393cef611f57d
David <zurek.dav@gmail.com>
2024-02-04 18:20:46 +0100

    Implement basic GraphQL server

---
+++ server.ts
@@ -0,0 +1,62 @@
+import { ApolloServer } from "@apollo/server"
+import { expressMiddleware } from "@apollo/server/express4"
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer"
+import { makeExecutableSchema } from "@graphql-tools/schema"
+import express from "express"
+import http from "http"
+import cors from "cors"
+import { Dav, Environment } from "dav-js"
+import { typeDefs } from "./src/typeDefs.js"
+import { resolvers } from "./src/resolvers.js"
+import "dotenv/config"
+
+const port = process.env.PORT || 4004
+const app = express()
+const httpServer = http.createServer(app)
+
+let schema = makeExecutableSchema({
+	typeDefs,
+	resolvers
+})
+
+const server = new ApolloServer({
+	schema,
+	plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+})
+
+await server.start()
+
+// Init dav
+let environment = Environment.Development
+
+switch (process.env.ENVIRONMENT) {
+	case "production":
+		environment = Environment.Production
+		break
+	case "staging":
+		environment = Environment.Staging
+		break
+}
+
+new Dav({
+	environment,
+	server: true
+})
+
+app.use(
+	"/",
+	cors<cors.CorsRequest>(),
+	express.json({ type: "application/json", limit: "50mb" }),
+	expressMiddleware(server, {
+		context: async ({ req }) => {
+			return {}
+		}
+	})
+)
+
+await new Promise<void>(resolve => httpServer.listen({ port }, resolve))
+console.log(`🚀 Server ready at http://localhost:${port}/`)
+
+BigInt.prototype["toJSON"] = function () {
+	return this.toString()
+}
